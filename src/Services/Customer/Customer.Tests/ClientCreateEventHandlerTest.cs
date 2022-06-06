5673d295ccb6b34a5c50e73e775535afdec3cf15
Daniel Argüeso <arg.dani@gmail.com>
2022-06-06 20:10:41 +0200

    Added unit test to Customer microservice

---
+++ src/Services/Customer/Customer.Tests/ClientCreateEventHandlerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Customer.Service.EventHandlers;
+using Customer.Service.EventHandlers.Commands;
+using Customer.Tests.Config;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Customer.Tests;
+
+[TestClass]
+public class ClientCreateEventHandlerTest
+{
+    private ILogger<ClientCreateEventHandler> GetLogger => new Mock<ILogger<ClientCreateEventHandler>>().Object;
+
+    [TestMethod]
+    public void TryToCreateClient()
+    {
+        var context = CustomerDbContextInMemory.Get();
+
+        var randomClientName = $"{Guid.NewGuid()}::{new Random().Next(0,9999):0000}::{Guid.NewGuid()}";
+
+        var handler = new ClientCreateEventHandler(context, GetLogger);
+
+        handler.Handle(
+            new ClientCreateCommand()
+            {
+                Name = randomClientName
+            },
+            new CancellationToken()).Wait();
+
+        var clients = context.Clients.Where(x => x.Name == randomClientName);
+
+        Assert.IsTrue(clients.Any() && clients.Count() == 1);
+    }
+
+}
+
---
+++ src/Services/Customer/Customer.Tests/Config/CustomerDbContextInMemory.cs
@@ -0,0 +1,19 @@
+﻿using Customer.Persistance.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Customer.Tests.Config
+{
+    public static class CustomerDbContextInMemory
+	{
+		public static CustomerDbContext Get()
+        {
+			var options = new DbContextOptionsBuilder<CustomerDbContext>()
+				.UseInMemoryDatabase(databaseName: "Customer.Db").
+				Options;
+
+			return new CustomerDbContext(options);
+				
+        }
+	}
+}
+
