e7d46d4cbc7212012880f26b02d5c0668d1d43fd
Daniel Argüeso <arg.dani@gmail.com>
2022-06-06 20:10:21 +0200

    Added logger to ClientCreateEventHandler

--- src/Services/Customer/Customer.Service.EventHandlers/ClientCreateEventHandler.cs
+++ src/Services/Customer/Customer.Service.EventHandlers/ClientCreateEventHandler.cs
@@ -2,16 +2,19 @@
 using Customer.Persistance.Database;
 using Customer.Service.EventHandlers.Commands;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Customer.Service.EventHandlers;
 
 public class ClientCreateEventHandler : INotificationHandler<ClientCreateCommand>
 {
     private readonly CustomerDbContext _context;
+    private readonly ILogger<ClientCreateEventHandler> _logger;
 
-    public ClientCreateEventHandler(CustomerDbContext context)
+    public ClientCreateEventHandler(CustomerDbContext context, ILogger<ClientCreateEventHandler> logger)
     {
         _context = context;
+        _logger = logger;
     }
 
     public async Task Handle(ClientCreateCommand notification, CancellationToken cancellationToken)
